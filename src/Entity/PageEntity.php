dcc6359c56c575169ca66638442de9ba615a3b8b
Sebastian Mößler <s.moeszler@binsoul.de>
2019-12-02 14:36:59 +0100

    Fix table name

--- src/Entity/PageEntity.php
+++ src/Entity/PageEntity.php
@@ -14,7 +14,7 @@ use Doctrine\ORM\Mapping as ORM;
  *
  * @ORM\Entity()
  * @ORM\Table(
- *     name="page_translation",
+ *     name="page",
  *     uniqueConstraints={
  *        @ORM\UniqueConstraint(columns={"route_id"}),
  *     }
