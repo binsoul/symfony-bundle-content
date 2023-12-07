f0f5f4a825a21f031bf3d0b4a17deff37e885ce0
Sebastian Mößler <s.moeszler@binsoul.de>
2023-12-07 14:27:30 +0100

    Add attribute to class

--- src/EventListener/TablePrefixListener.php
+++ src/EventListener/TablePrefixListener.php
@@ -5,7 +5,10 @@ declare(strict_types=1);
 namespace BinSoul\Symfony\Bundle\Content\EventListener;
 
 use BinSoul\Symfony\Bundle\Doctrine\EventListener\AbstractPrefixListener;
+use Doctrine\Bundle\DoctrineBundle\Attribute\AsDoctrineListener;
+use Doctrine\ORM\Events;
 
+#[AsDoctrineListener(event: Events::loadClassMetadata)]
 class TablePrefixListener extends AbstractPrefixListener
 {
     public function __construct(string $prefix)
